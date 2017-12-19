e6a0e7e4400d9c2e159d444d7ba98ea02ecc824c
corsair <xiangp126@126.com>
2017-12-19 05:26:42 -0500

    add window move help

--- tools/tmuxhelp.sh
+++ tools/tmuxhelp.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
 cat << "_EOF"
-
-************************************************************
-*                RE-ARRANGE PANE ORDER                     *
-************************************************************
+------------------------------------------------------------
+-                RE-ARRANGE PANE ORDER                     -
+------------------------------------------------------------
 bind-key    -T prefix       {                 swap-pane -U
 bind-key    -T prefix       }                 swap-pane -D
 
-************************************************************
-*          JOIN-PANE AND BREAK PANE TO WINDOW              *
-************************************************************
-bind-key    -T prefix       q                 display-panes
-bind-key    -T prefix       !                 break-pane
+------------------------------------------------------------
+-              SWAP AND MOVE FOR WINDOW                    -
+------------------------------------------------------------
+swap-window -s 3 -t 1          # swap window No.3 with No.1
+swap-window -t 1               # swap current window with No.1
+move-window -t 1               # move current window to No.1 
+                               # (No.1 must not be used now)
+
+------------------------------------------------------------
+-          JOIN-PANE AND BREAK PANE TO WINDOW              -
+------------------------------------------------------------
+bind-key    -T prefix       q                 display-pane order numbers
+bind-key    -T prefix       !                 break-pane into a new window
 :join-pane -s [session_name]:[window].[pane]  # join the pane to current window
 :join-pane -s virl:2.1
 
-************************************************************
-*                      MAXIMUM PANE                        *
-************************************************************
+------------------------------------------------------------
+-                      MAXIMUM PANE                        -
+------------------------------------------------------------
 bind-key    -T prefix       x                 close current pane
 bind-key    -T prefix       X                 toggle maximum pane
 
-************************************************************
-*                       SPLIT WINDOW                       *
-************************************************************
+------------------------------------------------------------
+-                       SPLIT WINDOW                       -
+------------------------------------------------------------
 bind-key    -T prefix       -                 split-window -v
 bind-key    -T prefix       |                 split-window -h
 
-************************************************************
-*                    FOR TMUX-RESURRECT                    *
-************************************************************
+------------------------------------------------------------
+-                    FOR TMUX-RESURRECT                    -
+------------------------------------------------------------
 bind-key    -T prefix       S       run-shell ~/.tmux/plugins/tmux-resurrect/scripts/save.sh
 bind-key    -T prefix       R       run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh
 bind-key    -T prefix       U       run-shell ~/.tmux/plugins/tpm/bindings/update_plugins
 
+------------------------------------------------------------
  _                        _          _
 | |_ _ __ ___  _   ___  _| |__   ___| |_ __
 | __| '_ ` _ \| | | \ \/ / '_ \ / _ \ | '_ \
