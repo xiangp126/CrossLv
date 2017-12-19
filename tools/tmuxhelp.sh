10c0eb6f984d697235ad73e9f63c30c241103967
= corsair <xiangp126@126.com>
2017-12-19 12:54:31 +0800

    change banner

--- tools/tmuxhelp.sh
+++ tools/tmuxhelp.sh
@@ -2,20 +2,38 @@
 
 cat << "_EOF"
 
-RE-ARRANGE PANE ORDER:
+************************************************************
+*                RE-ARRANGE PANE ORDER                     *
+************************************************************
 bind-key    -T prefix       {                 swap-pane -U
 bind-key    -T prefix       }                 swap-pane -D
 
+************************************************************
+*          JOIN-PANE AND BREAK PANE TO WINDOW              *
+************************************************************
 bind-key    -T prefix       q                 display-panes
 bind-key    -T prefix       !                 break-pane
+:join-pane -s [session_name]:[window].[pane]  # join the pane to current window
+:join-pane -s virl:2.1
 
-SPLIT WINDOW:
+************************************************************
+*                       SPLIT WINDOW                       *
+************************************************************
 bind-key    -T prefix       -                 split-window -v
 bind-key    -T prefix       |                 split-window -h
 
-For tmux-resurrect:
-bind-key    -T prefix       R                 run-shell /home/virl/.tmux/plugins/tmux-resurrect/scripts/restore.sh
-bind-key    -T prefix       S                 run-shell /home/virl/.tmux/plugins/tmux-resurrect/scripts/save.sh
-bind-key    -T prefix       U                 run-shell /home/virl/.tmux/plugins/tpm/bindings/update_plugins
+************************************************************
+*                    FOR TMUX-RESURRECT                    *
+************************************************************
+bind-key    -T prefix       S       run-shell ~/.tmux/plugins/tmux-resurrect/scripts/save.sh
+bind-key    -T prefix       R       run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh
+bind-key    -T prefix       U       run-shell ~/.tmux/plugins/tpm/bindings/update_plugins
+
+ _                        _          _
+| |_ _ __ ___  _   ___  _| |__   ___| |_ __
+| __| '_ ` _ \| | | \ \/ / '_ \ / _ \ | '_ \
+| |_| | | | | | |_| |>  <| | | |  __/ | |_) |
+ \__|_| |_| |_|\__,_/_/\_\_| |_|\___|_| .__/
+                                      |_|
 
 _EOF
