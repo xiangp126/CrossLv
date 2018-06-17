3010fafc89a7f198a8017a3df2dfd022e340f127
Peng Xiang <hi.pxiang@gmail.com>
2018-06-17 17:46:56 +0800

    update ycm-add-inc

--- tools/ycm_add_inc.sh
+++ tools/ycm_add_inc.sh
@@ -1,15 +1,78 @@
 #!/bin/bash
-
-incDir=inc
-if [[ -d "$incDir" ]]; then
-    echo "Already has directory inc, please check it"
-    exit
-fi
-
 set -x
+mainWd=$(cd $(dirname $0); pwd)
+startDir=`pwd`
+incDir=inc
 
-# nginx exclude dir
-ngxExDir=win32
-mkdir -p $incDir
-cd $incDir
-find .. -regex '.*.h$' ! -path '*inc*' ! -path '*win32*' -exec ln -s {} \;
+doJob() {
+    if [[ -d "$incDir" ]]; then
+        echo "Already has directory inc, please check it"
+        rm -rf $incDir
+        # exit
+    fi
+
+    mkdir -p $incDir
+    cd $incDir
+
+    fdPath=`which fd 2> /dev/null`
+
+        export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --no-ignore --exclude '.git'\
+                    --exclude '*.sw[p|o]' --exclude '*.[a|o]' --exclude '*.so'"
+
+    if [[ $fdPath != '' ]]; then
+        if [[ "$1" == 'nginx' ]]; then
+            cmd='fd --type f --no-ignore --exclude inc --exclude win32 '.*\.h\$' '$startDir
+        else
+            cmd='fd --type f --no-ignore --exclude inc '.*\.h\$' '$startDir
+        fi
+    else
+        # use legacy find
+        if [[ "$1" == 'nginx' ]]; then
+            cmd='find $startDir -regex '.*.h$' ! -path '*inc*' ! -path '*win32*''
+        else
+            cmd='find $startDir -regex '.*.h$' ! -path '*inc*''
+        fi
+    fi
+
+    # make soft link
+    for file in `$cmd`; do
+        ln -sf $file .
+    done
+
+    # copy default ycm config
+    cd $startDir
+    ycmConf=$HOME/.ycm_extra_conf.py
+    if [[ ! -f "$ycmConf" ]]; then
+        cp $ycmConf .
+    fi
+}
+
+usage() {
+    exeName=${0##*/}
+    cat << _EOF
+[NAME]
+    $exeName -- make soft link all headers of current directory to ./inc/
+
+[USAGE]
+    sh $exeName [nginx | help]
+
+[EXAMPLE]
+    sh $exeName
+    sh $exeName nginx --> only for generating Nginx source code
+
+_EOF
+}
+
+case $1 in
+    'nginx')
+        doJob nginx
+        ;;
+
+    'help')
+        usage
+        ;;
+
+    *)
+        doJob
+        ;;
+esac
