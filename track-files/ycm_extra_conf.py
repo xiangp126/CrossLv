a1a1f4099ecc163c78be52c152cecef0cac8527b
Peng Xiang <hi.pxiang@gmail.com>
2018-06-27 16:57:49 +0800

    change default ycm config to c

--- track-files/ycm_extra_conf.py
+++ track-files/ycm_extra_conf.py
@@ -34,19 +34,17 @@ flags = [
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c++11',
+'-std=c99',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
-'c++',
+'c',
 '-isystem',
 '/usr/include',
 '-isystem',
-'/usr/local/include',
-'-isystem',
-'/usr/include/c++/5',
+'/usr/include',
 '-I.',
 '-I./include',
 '-I./inc'
