a980513fd2723c757556dbd61a32b866166d5502
Peng Xiang <hi.pxiang@gmail.com>
2018-12-06 18:33:11 +0800

    shield C-Q for vim

--- track-files/ycm_extra_conf.py
+++ track-files/ycm_extra_conf.py
@@ -29,12 +29,13 @@ flags = [
 '-fexceptions',
 '-DNDEBUG',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-# '-std=c++11',
+# language to use when compiling headers. So it will guess. Badly.
+# So C++ headers will be compiled as C headers. You don't want that
+# so ALWAYS specify a "-std=<something>".
+# For a C project, you would set this to something like 'c99'
+# instead of 'c++11'.
+'-std=c++11',
+# '-std=c99',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
@@ -45,7 +46,7 @@ flags = [
 '/usr/include',
 '-I.',
 '-I./include',
-'-I./inc'
+'-I./inc',
 ]
 
 
