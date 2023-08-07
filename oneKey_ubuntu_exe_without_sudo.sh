47512a9bd4a8c5740e5b7b372e8e294a2f67be0f
PENG <hi.pxiang@gmail.com>
2023-08-06 22:34:28 -0300

    merge fix

--- oneKey_ubuntu_exe_without_sudo.sh
+++ oneKey_ubuntu_exe_without_sudo.sh
@@ -1,34 +1,88 @@
 #!/bin/bash
+# Only for ubuntu with sudo privilege
+# Life is hard, let's make code easier
 mainWd=$(cd $(dirname $0); pwd)
 trackedFileDir=./track-files
+trackedFiles=(
+    vimrc
+    bashrc
+    tmux.conf
+    gitconfig
+    gitignore
+)
 
-preInstallForUbuntu () {
+# prerequesites for ubuntu
+PrerequesForUbuntu=(
+    fd-find
+    ripgrep
+)
+
+InstallForUbuntu() {
     sudo apt-get update
-    sudo apt-get install \
-        fd-find -y
+    echo "sudo apt-get install -y ${PrerequesForUbuntu[@]}"
+
+    fdLinkLocation=/usr/local/bin/fd
+    if [ -L $fdLinkLocation ]; then
+        echo "fd link already exists, skip"
+        return
+    fi
+    cat << _EOF
+------------------------------------------------------
+Create fd link to fdfind
+------------------------------------------------------
+_EOF
+    sudo ln -s $(which fdfind) $fdLinkLocation
+    cat << _EOF
+------------------------------------------------------
+Relink sh to bash
+------------------------------------------------------
+_EOF
+    # check if soft link sh is linked to bash, if yes, skip. if not, link it
+    if [ -L /bin/sh ] && [ $(readlink /bin/sh) == "/bin/bash" ]; then
+        echo "sh is already linked to bash, skip"
+        return
+    fi
+    # link sh to bash
+    sudo ln -sf /bin/bash /bin/sh
 }
 
 handleTrackedFiles () {
-    # Deploy solarized
-    git clone https://github.com/altercation/vim-colors-solarized
-    cd vim-colors-solarized/colors
-    mkdir -p ~/.vim/colors
-    mv solarized.vim ~/.vim/colors/
+    cat << _EOF
+------------------------------------------------------
+Copy tracked files to home dir, including:
+    ${trackedFiles[@]}
+------------------------------------------------------
+_EOF
+    # copy tracked files to home dir and add .prefix to them
+    for file in ${trackedFiles[@]}; do
+        cp $trackedFileDir/$file ~/.$file
+    done
+}
 
-    trackedVimrc=$trackedFileDir/vimrc
-    trackedBashrc=$trackedFileDir/bashrc
-    trackedGitconfig=$trackedFileDir/gitconfig
-    trackedGitignore=$trackedFileDir/gitignore
-    cp $trackedVimrc $HOME/.vimrc
-    cp $trackedBashrc $HOME/.bashrc
-    cp $trackedGitconfig $HOME/.gitconfig
-    cp $trackedGitignore $HOME/.gitignore
+installVimPlug () {
+    cat << _EOF
+------------------------------------------------------
+COPY TRACKED FILES TO HOME DIR, INCLUDING:
+    ${trackedFiles[@]}
+------------------------------------------------------
+_EOF
+    if [ -d ~/.vim/autoload ]; then
+        # Update the plugins
+        vim +PlugUpdate +qall
+        return
+    fi
+
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    # install vim plugins
+    vim +PlugInstall +qall
 }
 
 install () {
     preInstallForUbuntu
+    installVimPlug
     handleTrackedFiles
 }
 
 install
-
