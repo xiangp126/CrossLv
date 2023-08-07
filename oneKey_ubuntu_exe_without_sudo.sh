0030d45dbc95920e6ae91716a7683396ec3941b0
Peter X <hipeter@gmail.com>
2023-08-06 19:49:21 -0700

    correct and add more plugins

--- oneKey_ubuntu_exe_without_sudo.sh
+++ oneKey_ubuntu_exe_without_sudo.sh
@@ -2,7 +2,8 @@
 # Only for ubuntu with sudo privilege
 # Life is hard, let's make code easier
 mainWd=$(cd $(dirname $0); pwd)
-trackedFileDir=./track-files
+trackedFileDir=$mainWd/track-files
+downloadDir=$mainWd/Downloads
 trackedFiles=(
     vimrc
     bashrc
@@ -10,48 +11,56 @@ trackedFiles=(
     gitconfig
     gitignore
 )
-downloadDir=~/Downloads
 
 # prerequesites for ubuntu
 PrerequesForUbuntu=(
     fd-find
     ripgrep
+    universal-ctags
 )
 
-InstallForUbuntu() {
+installForUbuntu() {
     sudo apt-get update
     echo "sudo apt-get install -y ${PrerequesForUbuntu[@]}"
 
+    installVimPlug
+    createFdLinkToFdfind
+    relinkShToBash
+}
+
+createFdLinkToFdfind() {
+    cat << _EOF
+Create fd link to fdfind
+------------------------------------------------------
+_EOF
     fdLinkLocation=/usr/local/bin/fd
     if [ -L $fdLinkLocation ]; then
         echo "fd link already exists, skip"
         return
     fi
-    cat << _EOF
-------------------------------------------------------
-Create fd link to fdfind
-------------------------------------------------------
-_EOF
+
     sudo ln -s $(which fdfind) $fdLinkLocation
+}
+
+relinkShToBash() {
     cat << _EOF
 ------------------------------------------------------
 Relink sh to bash
-------------------------------------------------------
 _EOF
-    # check if soft link sh is linked to bash, if yes, skip. if not, link it
     if [ -L /bin/sh ] && [ $(readlink /bin/sh) == "/bin/bash" ]; then
         echo "sh is already linked to bash, skip"
         return
     fi
+
     # link sh to bash
     sudo ln -sf /bin/bash /bin/sh
+}
+
+installSolarizedColorScheme() {
     cat << _EOF
 ------------------------------------------------------
-Manual install and deploy colorized color scheme for vim https://github.com/altercation/vim-colors-solarized
-------------------------------------------------------
+Install Solarized Color Scheme for VIM
 _EOF
-    git clone $downloadDir/https://github.com/altercation/vim-colors-solarized
-
     if [ -f ~/.vim/colors/solarized.vim ]; then
         echo "solarized.vim already exists, skip"
         return
@@ -61,31 +70,23 @@ _EOF
         mkdir -p ~/.vim/colors
     fi
 
-    cp vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+    solarizedSrc=$HOME/.vim/bundle/vim-colors-solarized/colors/solarized.vim
+    cp  $solarizedSrc $HOME/.vim/colors/
 }
 
-handleTrackedFiles () {
+handleTrackedFiles() {
     cat << _EOF
 ------------------------------------------------------
 Copy tracked files to home dir, including:
     ${trackedFiles[@]}
-------------------------------------------------------
 _EOF
-    # copy tracked files to home dir and add .prefix to them
     for file in ${trackedFiles[@]}; do
         cp $trackedFileDir/$file ~/.$file
     done
 }
 
-installVimPlug () {
-    cat << _EOF
-------------------------------------------------------
-COPY TRACKED FILES TO HOME DIR, INCLUDING:
-    ${trackedFiles[@]}
-------------------------------------------------------
-_EOF
+installVimPlug (){
     if [ -d ~/.vim/autoload ]; then
-        # Update the plugins
         vim +PlugUpdate +qall
         return
     fi
@@ -95,11 +96,12 @@ _EOF
 
     # install vim plugins
     vim +PlugInstall +qall
+
+    installSolarizedColorScheme
 }
 
 install () {
     installForUbuntu
-    installVimPlug
     handleTrackedFiles
 }
 
