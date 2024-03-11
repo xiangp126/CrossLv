2a64630058f763125d5f3e9608136b65ddd7ef68
Peter X <hi.pxiang@gmail.com>
2024-03-11 15:50:58 -0700

    add checker for exe boot up script

--- template/vscode_max_user_watches.sh
+++ template/vscode_max_user_watches.sh
@@ -1,5 +1,19 @@
-sudo cat /proc/sys/fs/inotify/max_user_watches
-echo "Change..."
-sudo sysctl fs.inotify.max_user_watches=524288
-sudo cat /proc/sys/fs/inotify/max_user_watches
+#!/bin/bash
 
+current_value=$(cat /proc/sys/fs/inotify/max_user_watches)
+desired_value=524288
+if [ "$current_value" -ge "$desired_value" ]; then
+    echo "Max file watchers is already set to $desired_value!"
+    exit 0
+fi
+
+echo "Change the max file watchers to $desired_value ..."
+
+sudo sysctl fs.inotify.max_user_watches=524288
+if [ $? -ne 0 ]; then
+    echo "Failed to change the max file watchers!"
+    exit 1
+fi
+
+cat /proc/sys/fs/inotify/max_user_watches
+echo "Max file watchers changed successfully!"
